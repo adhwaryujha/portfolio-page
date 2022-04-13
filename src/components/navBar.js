7def5a3e4a815e444efb8f5ab6e3d024f5e27b5a
Adhwaryu Jha <adhwaryujha@gmail.com>
2022-04-14 00:38:50 +0530

    Day 1 of creating portfolio

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,3 +1,17 @@
-const navBar = () => {};
+import { Fragment } from "react";
 
-export default navBar;
+const NavBar = () => {
+  return (
+    <Fragment>
+      <div>Adhwaryu Jha</div>
+      <div>
+        <span>About</span>
+        <span>Project</span>
+        <span>experience</span>
+        <span>connect with me!!!</span>
+      </div>
+    </Fragment>
+  );
+};
+
+export default NavBar;
